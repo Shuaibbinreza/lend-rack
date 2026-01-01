23032087552e8ad4d49d67237f168f5ba35a4503
DESKTOP-LA3KBVD\It <shuaib6950@gmail.com>
2026-01-01 10:02:49 +0600

    make form for book entry

--- src/main/java/com/lendrack/lend_rack/controller/web/BookController.java
+++ src/main/java/com/lendrack/lend_rack/controller/web/BookController.java
@@ -6,11 +6,17 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
+import com.lendrack.lend_rack.model.domain.Author;
 import com.lendrack.lend_rack.model.domain.Book;
+import com.lendrack.lend_rack.model.dto.CreateBookRequest;
+import com.lendrack.lend_rack.service.AuthorService;
 import com.lendrack.lend_rack.service.BookService;
 
 import lombok.RequiredArgsConstructor;
@@ -20,6 +26,7 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/books")
 public class BookController {
     private final BookService bookService;
+    private final AuthorService authorService;
 
     @GetMapping
     public String index(Model model) {
@@ -35,5 +42,21 @@ public class BookController {
         List<Book> books = bookService.getAllBooks(pageable);
         model.addAttribute("books", books);
         return "manage_books";
+    }
+
+    @GetMapping("manage-books/create")
+    public String createBookForm(Model model) {
+        Pageable pageable = PageRequest.of(0, 100, Sort.Direction.ASC, "name");
+        List<Author> authors = authorService.findAllAuthors(pageable);
+        model.addAttribute("authors", authors);
+        model.addAttribute("book", new CreateBookRequest("", 0L, null, null, null, "", 0));
+        return "create_book";
+    }
+
+    @PostMapping("manage-books/create")
+    public String createBook(@ModelAttribute("book") CreateBookRequest request, RedirectAttributes redirectAttributes) {
+        bookService.create(request);
+        redirectAttributes.addFlashAttribute("message", "Book created successfully");
+        return "redirect:/books/manage-books/all";
     }
 }
